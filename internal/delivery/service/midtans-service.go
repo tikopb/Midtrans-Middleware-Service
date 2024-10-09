c4d13cdc30578e3650aa6e8c3d420887d6b559e2
“TIKO <tiko.pb@gmail.com>
2024-10-10 06:48:03 +0700

    fix bug on response message note: please fix on handler

--- internal/delivery/service/midtans-service.go
+++ internal/delivery/service/midtans-service.go
@@ -2,16 +2,17 @@ package service
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"github.com/tikopb/Midtrans-Middleware-Service/internal/model"
-	"io/ioutil"
+	"io"
 	"net/http"
 
 	master "github.com/tikopb/Midtrans-Middleware-Service/internal/main-module"
 )
 
 type Repository interface {
-	//CreatePayment
 	CreatePaymentLink(request model.MidtransPaymentLinkRequest) (model.MidtransPaymentLinkRespont, error)
 }
 
@@ -26,7 +27,7 @@ func GetRepository(master master.Repository) Repository {
 }
 
 func (m *midtrans) CreatePaymentLink(request model.MidtransPaymentLinkRequest) (model.MidtransPaymentLinkRespont, error) {
-	midtrans_base_url := m.master.GetEnvVariabel("midtrans_base_url") + "v1/payment-links'"
+	midtransBaseUrl := m.master.GetEnvVariabel("midtrans_base_url") + "v1/payment-links"
 	serverKey := m.master.GetEnvVariabel("serverKey")
 	method := "POST"
 
@@ -37,14 +38,17 @@ func (m *midtrans) CreatePaymentLink(request model.MidtransPaymentLinkRequest) (
 	}
 
 	client := &http.Client{}
-	req, err := http.NewRequest(method, midtrans_base_url, bytes.NewBuffer(jsonData))
+	req, err := http.NewRequest(method, midtransBaseUrl, bytes.NewBuffer(jsonData))
 	if err != nil {
 		return model.MidtransPaymentLinkRespont{}, err
 	}
 
+	// Encode the serverKey for Basic Auth
+	encodedServerKey := base64.StdEncoding.EncodeToString([]byte(serverKey + ":"))
+
 	// Set headers
 	req.Header.Add("accept", "application/json")
-	req.Header.Add("authorization", "Basic "+serverKey)
+	req.Header.Add("authorization", "Basic "+encodedServerKey)
 	req.Header.Add("content-type", "application/json")
 
 	// Send request
@@ -54,19 +58,25 @@ func (m *midtrans) CreatePaymentLink(request model.MidtransPaymentLinkRequest) (
 	}
 	defer res.Body.Close()
 
+	// Check for non-2xx status codes
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		// Read response body to see the error message
+		bodyBytes, _ := io.ReadAll(res.Body)
+		return model.MidtransPaymentLinkRespont{}, fmt.Errorf("received non-2xx response code: %d, body: %s", res.StatusCode, string(bodyBytes))
+	}
+
 	// Read response
-	bodyBytes, err := ioutil.ReadAll(res.Body)
+	bodyBytes, err := io.ReadAll(res.Body)
 	if err != nil {
 		return model.MidtransPaymentLinkRespont{}, err
 	}
 
-	// Parse the response into the MidtransPaymentLinkRespont struct
+	// Unmarshal the response body into your response struct
 	var response model.MidtransPaymentLinkRespont
 	err = json.Unmarshal(bodyBytes, &response)
 	if err != nil {
 		return model.MidtransPaymentLinkRespont{}, err
 	}
 
-	//TODO implement mef
 	return response, nil
 }
